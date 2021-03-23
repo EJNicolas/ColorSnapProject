b402f8c29cdec63f5e5ec954619ab3a2290ca69a
Kevin <kevinyu202@gmail.com>
2021-03-22 22:30:17 -0700

    Comments
    added comments

--- app/src/main/java/com/example/colorsnap/CameraActivity.java
+++ app/src/main/java/com/example/colorsnap/CameraActivity.java
@@ -17,7 +17,10 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+//Activity for displaying, creating,and operating the camera to take a picture
 public class CameraActivity extends Activity implements View.OnClickListener {
+
+    //Create variables
     Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
     Button buttonEditColor;
     Button cameraBtn;
@@ -26,6 +29,8 @@ public class CameraActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Getting shared preference data to decide if light or dark theme will be used
         SharedPreferences sharedPrefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
         if(!(sharedPrefs==null)){
             boolean dark = sharedPrefs.getBoolean("darkMode", false);
@@ -40,12 +45,19 @@ public class CameraActivity extends Activity implements View.OnClickListener {
             setTheme(R.style.LightTheme);
         }
         setContentView(R.layout.activity_camera);
+
+        //Initialize variables
+
         selectedImage = findViewById(R.id.imageView);
         cameraBtn = findViewById(R.id.cameraBtn);
         buttonEditColor = (Button) findViewById(R.id.buttonEditColor);
+
+        //Set listeners
+
         buttonEditColor.setOnClickListener(this);
         cameraBtn.setOnClickListener(this);
 
+//Grants permission toe access the camera if the correct request code is called
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 100);
@@ -57,6 +69,8 @@ public class CameraActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+
+       //If statements for when the edit color button is press to start the edit color activity otherwise if the camera button is pressed, read a toast message and start the camera activity
         if (buttonEditColor.isPressed()) {
             Intent i = new Intent(this, EditColorActivity.class);
             startActivity(i);
@@ -68,6 +82,8 @@ public class CameraActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+      // if the request code is correct, capture the image from the camera
         if (requestCode == 100) {
 
             Bitmap captureImage = (Bitmap) data.getExtras().get("data");
